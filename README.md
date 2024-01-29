840694ec75e92c79850d89dff3c2fba92c6f4e28
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2024-01-29 18:09:44 -0500

    add some functions

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Node.js README
+
+## Description
+this a dry run
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+<!-- Add installation instructions here -->
+
+## Usage
+<!-- Add usage information here -->
+
+## License
+This project is licensed under the MIT License.
+
+## Contributing
+<!-- Add contribution guidelines here -->
+
+## Tests
+<!-- Add test instructions here -->
+
+## Questions
+For questions, please contact undefined.
+GitHub: [undefined](https://github.com/undefined)
+  
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to prompt user for project information
+async function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the project title:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a brief description of your project:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project:',
+            choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause'],
+        },
+        // Add more prompts based on the acceptance criteria
+    ]);
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Main function
+async function init() {
+    try {
+        const answers = await promptUser();
+        const readmeContent = generateReadme(answers);
 
-// Function call to initialize app
-init();
+        // Save to file example
+        fs.writeFileSync('README.md', readmeContent);
+
+        console.log('README.md generated successfully!');
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
